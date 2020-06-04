6a3838f689174b437a53578ad82711ff3241b459
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-06-04 02:25:27 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,11 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.string "theme"
-    t.float "price"
-    t.boolean "family_friendly"
-    t.date "opening_date"
-    t.date "closing_date"
-    t.string "long_description"
+    t.integer "costume_inventory"
+    t.integer "num_of_employees"
+    t.boolean "still_in_business"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
   end
 
   create_table "costumes", force: :cascade do |t|
@@ -32,14 +31,15 @@ ActiveRecord::Schema.define(version: 3) do
     t.datetime "updated_at"
   end
 
-  create_table "costumestores", force: :cascade do |t|
+  create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.integer "costume_inventory"
-    t.integer "num_of_employees"
-    t.boolean "still_in_business"
-    t.time "opening_time"
-    t.time "closing_time"
+    t.string "theme"
+    t.float "price"
+    t.boolean "family_friendly"
+    t.date "opening_date"
+    t.date "closing_date"
+    t.text "description"
   end
 
 end
